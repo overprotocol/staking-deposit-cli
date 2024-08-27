248f2ccf9b610628cd12e3df76d4ce2b5fc03bc2
syjn99 <syjn99@gmail.com>
2024-08-27 14:26:23 +0900

    Exclude 'mainnet' in prompt

--- staking_deposit/settings.py
+++ staking_deposit/settings.py
@@ -1,7 +1,7 @@
 from typing import Dict, NamedTuple
 from eth_utils import decode_hex
 
-DEPOSIT_CLI_VERSION = '2.6.0'
+DEPOSIT_CLI_VERSION = '2.6.1'
 
 
 class BaseChainSetting(NamedTuple):
@@ -11,29 +11,13 @@ class BaseChainSetting(NamedTuple):
 
 
 MAINNET = 'mainnet'
-GOERLI = 'goerli'
-PRATER = 'prater'
-SEPOLIA = 'sepolia'
-ZHEJIANG = 'zhejiang'
 OVER = 'over'
 OVER_DOLPHIN = 'over_dolphin'
 
-# Mainnet setting
+# Mainnet setting (Ethereum Mainnet)
 MainnetSetting = BaseChainSetting(
     NETWORK_NAME=MAINNET, GENESIS_FORK_VERSION=bytes.fromhex('00000000'),
     GENESIS_VALIDATORS_ROOT=bytes.fromhex('4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95'))
-# Goerli setting
-GoerliSetting = BaseChainSetting(
-    NETWORK_NAME=GOERLI, GENESIS_FORK_VERSION=bytes.fromhex('00001020'),
-    GENESIS_VALIDATORS_ROOT=bytes.fromhex('043db0d9a83813551ee2f33450d23797757d430911a9320530ad8a0eabc43efb'))
-# Sepolia setting
-SepoliaSetting = BaseChainSetting(
-    NETWORK_NAME=SEPOLIA, GENESIS_FORK_VERSION=bytes.fromhex('90000069'),
-    GENESIS_VALIDATORS_ROOT=bytes.fromhex('d8ea171f3c94aea21ebc42a1ed61052acf3f9209c00e4efbaaddac09ed9b8078'))
-# Zhejiang setting
-ZhejiangSetting = BaseChainSetting(
-    NETWORK_NAME=ZHEJIANG, GENESIS_FORK_VERSION=bytes.fromhex('00000069'),
-    GENESIS_VALIDATORS_ROOT=bytes.fromhex('53a92d8f2bb1d85f62d16a156e6ebcd1bcaba652d0900b2c2f387826f3481f6f'))
 # Over Setting
 OverSetting = BaseChainSetting(
     NETWORK_NAME=OVER, GENESIS_FORK_VERSION=bytes.fromhex('00000018'),
@@ -52,7 +36,7 @@ ALL_CHAINS: Dict[str, BaseChainSetting] = {
 }
 
 
-def get_chain_setting(chain_name: str = MAINNET) -> BaseChainSetting:
+def get_chain_setting(chain_name: str = OVER) -> BaseChainSetting:
     return ALL_CHAINS[chain_name]
 
 
